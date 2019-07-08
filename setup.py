9fc3a608624ca999f1446e74d5e4ba9b11543c4a
alexsilva <geniofuturo@gmail.com>
2019-07-08 18:37:19 -0300

    setup install data.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     license='BSD',
     keywords='supervisor dashboard remote control'.split(),
     platforms='any',
-    package=['dashvisor'],
+    packages=['dashvisor', 'dashvisor.backends'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
